74469f9c7e289a71dcafc6d3fbd366cbc6c83762
user <user@example.com>
2021-02-12 05:05:59 -0500

    Better way to loop through packages to install them

--- first-boot.sh
+++ first-boot.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+PKGS=(
+	xorg
+	xorg-xinit
+	git
+	xmonad
+	xmonad-contrib
+	termite
+	epiphany
+)
+
 echo "--Installing Packages--"
-sudo pacman -S vim git xorg xorg-xinit xmonad xmonad-contrib termite epiphany --noconfirm 
+for PKG in "${PKGS[@]}"; do
+	echo "Installing $PKG"
+	sudo pacman -S "$PKG" --noconfirm
+done
 
 echo "--Getting Dotfiles--"
 cd /home/person
