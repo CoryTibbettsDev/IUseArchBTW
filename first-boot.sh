b5b5fe8b3aa2f4d92a8cec81eb018bc8a7e9ee12
user <user@example.com>
2021-02-16 15:18:54 -0500

    Added paru and librewolf install

--- first-boot.sh
+++ first-boot.sh
@@ -12,20 +12,25 @@ PKGS=(
 	dmenu
 	# Utilities
 	git
-	curl
+	neofetch
+	htop
 	# Wallpaper manager
 	nitrogen
+	# Terminal emulator
 	termite
 	# Web browser
 	epiphany
 	# Audio control
-	pulseaudio
+	alsa
+	alsa-utils
 	# Video player
 	mpv
 	# PDF viewer
 	xpdf
 	# zathura
 	# groff
+	# Auto mount external devices
+	udiskie
 	# GUI file browser
 	pcmanfm
 )
@@ -35,6 +40,19 @@ for PKG in "${PKGS[@]}"; do
 	echo "Installing $PKG"
 	sudo pacman -S "$PKG" --noconfirm
 done
+
+echo "--Installing Paru--"
+# Install paru Arch User Rrpository helper
+cd ~
+sudo pacman -S --needed base-devel --noconfirm
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+cd ~
+
+echo "--Installing Librewolf--"
+# Install librewolf browser
+paru -S librewolf-bin
 
 echo "--Getting Dotfiles--"
 cd /home/person
