459961846743d1c4b0cb21dd69b7a3eb21b5f4c1
user <user@example.com>
2021-03-24 04:40:25 -0400

    Switch to printf and tee

--- packages.sh
+++ packages.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 # Script for installing all the packages on my system
+
+# Would like to detect the hardware and install based on that
 
 PACKAGES=(
 	# Text Editors ####
@@ -43,44 +45,45 @@ PACKAGES=(
 	# Needed for ytfzf menu
 	fzf
 
-    #### DRIVERS ####
-    # https://github.com/lutris/docs/blob/master/InstallingDrivers.md
-    # If you want to run 32 bit applications install the 32 bit packages
-    # edit /etc/pacman.conf and uncomment the mutlilib mirror list
+	#### DRIVERS ####
+	# https://github.com/lutris/docs/blob/master/InstallingDrivers.md
+	# If you want to run 32 bit applications install the 32 bit packages
+	# edit /etc/pacman.conf and uncomment the mutlilib mirror list
+	# Also need these for wine
 
-    # vulkan-validation-layers
-    ## AMD
-    # vulkan-radeon
-    # vulkan-icd-loader
-    ## 32 bit AMD
-    # lib32-mesa
-    # lib32-vulkan-radeon
-    # lib32-vulkan-icd-loader
+	# vulkan-validation-layers
+	## AMD
+	# vulkan-radeon
+	# vulkan-icd-loader
+	## 32 bit AMD
+	# lib32-mesa
+	# lib32-vulkan-radeon
+	# lib32-vulkan-icd-loader
 
-    ## Nvidia
-    # nvidia-dkms
-    # nvidia-utils
-    # nvidia-settings
-    # vulkan-icd-loader
-    ## 32 bit Nvidia
-    # lib32-nvidia-utils
-    # lib32-vulkan-icd-loader
+	## Nvidia
+	# nvidia-dkms
+	# nvidia-utils
+	# nvidia-settings
+	# vulkan-icd-loader
+	## 32 bit Nvidia
+	# lib32-nvidia-utils
+	# lib32-vulkan-icd-loader
 
-    ## Intel
-    # vulkan-intel
-    # vulkan-icd-loader
-    ## 32 bit Intel
-    # lib32-mesa
-    # lib32-vulkan-intel
-    # lib32-vulkan-icd-loader
+	## Intel
+	# vulkan-intel
+	# vulkan-icd-loader
+	## 32 bit Intel
+	# lib32-mesa
+	# lib32-vulkan-intel
+	# lib32-vulkan-icd-loader
 )
-echo "--Installing Packages--"
+printf "--Installing Packages--"
 for PKG in "${PACKAGES[@]}"; do
-	echo "Installing $PKG"
+	printf "Installing $PKG"
 	sudo pacman -S "$PKG" --noconfirm
 done
 
-echo "--Installing Paru--"
+printf "--Installing Paru--"
 # Install paru Arch User Rrpository helper
 cd ~
 sudo pacman -S base-devel --noconfirm
@@ -89,7 +92,7 @@ cd paru
 makepkg -si
 cd ~
 
-echo "--Installing Librewolf--"
+printf "--Installing Librewolf--"
 # Install librewolf browser
 paru -S librewolf-bin --noconfirm
 
@@ -97,14 +100,14 @@ paru -S librewolf-bin --noconfirm
 # Dependencies are youtube-dl, mpv, jq, (optional for a menu) fzf,
 # (optional for thumbnails) ueberzug
 # Source code: https://github.com/pystardust/ytfzf
-echo "--Installing ytfzf--"
+printf "--Installing ytfzf--"
 paru -S ytfzf-git --noconfirm
 
 # Change swappiness to better value
 sudo sysctl vm.swappiness=10
-echo "vm.swappiness=10" > /etc/sysctl.d/99-swappiness.conf
+printf "vm.swappiness=10" | sudo tee -a /etc/sysctl.d/99-swappiness.conf
 
-echo "--Getting Dotfiles--"
+printf "--Getting Dotfiles--"
 cd ~
 git clone https://github.com/CoryTibbettsDev/.dotfiles
 cd .dotfiles
